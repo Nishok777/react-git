838ff4f814e810de90e5b7a446a49ce6dc492467
Nishok777 <125253923+Nishok777@users.noreply.github.com>
2023-11-23 14:41:38 +0530

    Add files via upload

---
+++ day 6 phm/src/App.js
@@ -0,0 +1,9 @@
+
+import Table from './Table';
+const App=()=>
+{
+  return(
+    <Table name={'NISHOK'} roll={'7463'} mail={'kfsak@gmail.com'} cgpa={'9.2'} contact={'8373882827'}/>
+  )
+}
+export default App;
